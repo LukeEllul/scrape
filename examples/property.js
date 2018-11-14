e84d00fb81f5dfd7c39adc1ca7533313766f41ce
LukeEllul <ellul.luke@gmail.com>
2018-11-14 10:55:28 +0100

    added property example

--- examples/property.js
+++ examples/property.js
@@ -5,16 +5,22 @@ const { injectJquery, injectScript, injectUrlScript, injectRamda } = require('..
 const { scrape } = require('../src/scrape/scrape');
 const { makeDriver } = require('../src/selenium/selenium');
 
-const Driver = makeDriver('MicrosoftEdge');
+const Driver = makeDriver('chrome');
+
+const url = `https://franksalt.com.mt/advanced-search/?status=forSale&bedrooms=&priceFrom=&priceTo=&referenceNum=`;
 
 apply(
     Driver.build(),
     Promise.resolve(2),
     scrape(
-        `https://www.bing.com/`,
+        url,
         [],
         function(v, cb){
-            cb(R.range(0, 56).map(n => n * v))
+            const links = $(`section[role="main"] > div[class="properties-container properties-container- clearfix"] > div[class="row"] > div[class="col-xs-12 col-md-6 property-grid-item"] > a`)
+                .map(function(){
+                    return $(this).attr("href")
+                }).get();
+            cb(links);
         }
     ),
     driver => promise => promise
